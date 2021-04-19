99f0e3a41113ae655a78f81736cc9052be8ce051
futuregit <tomcis102@hotmail.com>
2021-04-18 22:44:20 -0400

    Added documentation to the context

--- src/pages/collection/collection.component.jsx
+++ src/pages/collection/collection.component.jsx
@@ -6,10 +6,22 @@ import CollectionsContext from '../../contexts/collections/collections.context';
 
 import './collection.styles.scss';
 
+// this.props.match object is given to use by the BrowserRouter
+// wrapped around the root App component in index.js.
+// The match object contains the path, url, and params objects.
+
 const CollectionPage = ({ match }) => {
+  // collections will hold an array of a collection title and a array of collection items by title
   const collections = useContext(CollectionsContext);
+  // match.params.collectionId is either "hats", "shoes", 
+  // "jacket", "mens" or "women".
+
   const collection = collections[match.params.collectionId];
+  // Grab the title and items by collection title.
   const { title, items } = collection;
+  // Use items.map to return an array with item.id(1,2,3...)
+  // and item({id: 1, imageUrl: "https...", name: "Brown Hat", price: 25} })
+  // and assign each index or array items as props to CollectionItem.
   return (
     <div className='collection-page'>
       <h2 className='title'>{title}</h2>
--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -4,6 +4,9 @@ import { Route } from 'react-router-dom';
 import CollectionsOverview from '../../components/collections-overview/collections-overview.component';
 import CollectionPage from '../collection/collection.component';
 
+// Display first Route for a overview page for shop url
+// Display second Route for a collection title such as all hats.
+// The second route url is like shop/hats
 const ShopPage = ({ match }) => (
   <div className='shop-page'>
     <Route exact path={`${match.path}`} component={CollectionsOverview} />
